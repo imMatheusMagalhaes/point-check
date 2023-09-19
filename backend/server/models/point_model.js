490695a355c8f4320539a9810cb3a488b0198d6e
Matheus Magalhães <magalhaes.almeida.matheus@gmail.com>
2023-09-19 10:55:46 -0300

    feat: format date with getter

--- backend/server/models/point_model.js
+++ backend/server/models/point_model.js
@@ -1,19 +1,35 @@
 import mongoose from "mongoose"
+import moment from 'moment'
+moment.locale('pt-br');
 
 const PointSchema = new mongoose.Schema({
+	projects: {
+		type: Array,
+		required: true,
+	},
 	inputTime: {
 		type: Date,
 		required: true,
+		get: (date) => moment(new Date(date)).format('LT')
 	},
 	outputTime: {
 		type: Date,
+		get: (date) => moment(new Date(date)).format('LT')
 	},
-	projects: {
-		type: Array
-	}
+	createdAt: {
+		type: Date,
+		get: (date) => moment(new Date(date)).format('L'),
+	},
+	updatedAt: {
+		type: Date,
+		get: (date) => moment(new Date(date)).format('L')
+	},
 }, {
 	timestamps: true,
-	versionKey: false
+	versionKey: false,
+	toJSON: { getters: true },
+	toObject: { getters: true },
+	id: false
 });
 
 const Point = mongoose.model("Point", PointSchema);
