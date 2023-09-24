68bb3cf4ded1ce91546b638baca552422e952315
Matheus Magalhães <magalhaes.almeida.matheus@gmail.com>
2023-09-23 21:01:03 -0300

    fix: set input time error

--- backend/server/services/point_service.js
+++ backend/server/services/point_service.js
@@ -1,60 +1,67 @@
-import moment from 'moment'
-import Point from '../models/point_model';
-moment.locale('pt-br');
+import moment from "moment";
+import Point from "../models/point_model";
+moment.locale("pt-br");
 
-const get_current_time = () => moment()
+const get_current_time = () => moment();
 
-const format_time = (time) => moment(time).format('lll')
+const format_time = (time) => moment(time).format("lll");
 
-const get_inputs_without_output = async () => await Point.findOne({
-	outputTime: {
-		$exists: false
-	}
-})
+const get_inputs_without_output = async () =>
+	await Point.findOne({
+		outputTime: {
+			$exists: false,
+		},
+	});
 
 const create = async (body) => {
-	const point = await new Point(body).save()
-	return point
-} 
+	const point = await new Point(body).save();
+	return point;
+};
 
 const set_input = async (body) => {
-	const inputs_without_output = await get_inputs_without_output()
-	const current_time = moment(body?.time) ?? get_current_time()
-	const projects = body?.projects ?? []
+	const inputs_without_output = await get_inputs_without_output();
+	const current_time = moment(body?.time) ?? get_current_time();
+	const projects = body?.projects ?? [];
 	if (!inputs_without_output) {
 		const point = await new Point({
 			inputTime: current_time,
-			projects
-		}).save()
-		return { message: "entrada registrada.", time: format_time(point.inputTime) }
-	} else return { message: "entrada já registrada hoje." }
-}
+			projects,
+		}).save();
+		return {
+			message: "entrada registrada.",
+			time: format_time(current_time),
+		};
+	} else return { message: "entrada já registrada hoje." };
+};
 
 const set_output = async (body) => {
-	const lastPoint = await get_inputs_without_output()
+	const lastPoint = await get_inputs_without_output();
 	if (lastPoint) {
-		const current_time = moment(body?.time) ?? get_current_time()
-		await Point.updateOne({ _id: lastPoint._id }, { outputTime: current_time })
-		return { message: "saida registrada.", time: format_time(current_time) }
-	} else return { message: "nenhuma entrada registrada." }
-}
+		const current_time = moment(body?.time) ?? get_current_time();
+		await Point.updateOne({ _id: lastPoint._id }, { outputTime: current_time });
+		return { message: "saida registrada.", time: format_time(current_time) };
+	} else return { message: "nenhuma entrada registrada." };
+};
 
 const update_projects = async (body) => {
-	const { projects } = body
-	const lastPoint = await get_inputs_without_output()
+	const { projects } = body;
+	const lastPoint = await get_inputs_without_output();
 	if (lastPoint) {
-		const db_projects = [...lastPoint.projects, ...projects]
-		await Point.updateOne({ _id: lastPoint._id }, { projects: db_projects })
-		return { message: `${lastPoint._id} - atualizado` }
-	} else return { message: "nenhuma entrada registrada." }
-}
+		const db_projects = [...lastPoint.projects, ...projects];
+		await Point.updateOne({ _id: lastPoint._id }, { projects: db_projects });
+		return { message: `${lastPoint._id} - atualizado` };
+	} else return { message: "nenhuma entrada registrada." };
+};
 
 const get_all = async () => {
-	return await Point.find().sort({ createdAt: 1 })
-}
+	return await Point.find().sort({ createdAt: 1 });
+};
 
-
-
-
-
-export default { create, set_input, set_output, get_all, update_projects, get_inputs_without_output }
+export default {
+	create,
+	set_input,
+	set_output,
+	get_all,
+	update_projects,
+	get_inputs_without_output,
+};
